13be44b27ee004f52592966f205cdda6d88b8d5b
Beatriz Garcia <beatrizgarcia.jim@gmail.com>
2019-01-14 18:27:58 +0100

    add dataCard state

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,29 @@ import React, { Component } from 'react';
 import CardGenerator from './CardGenerator';
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+          dataCard:{
+            pallete: "",
+            typography: "",
+            name: "",
+            job: "",
+            photo: "",
+            email: "",
+            phone: "",
+            linkedin: "",
+            github: "",
+            skills: [],
+            success: "",
+            cardURL: "",
+            error: ""
+          }
+        }
+    }
+    
     render(){
-        return <CardGenerator />
+        return <CardGenerator dataCard={this.state.dataCard} />
     }
 }
 
