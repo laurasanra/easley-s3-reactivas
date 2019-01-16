41d807bd442190f97cb99f212cab091a1feba520
laurasanra <laura.san.rada@gmail.com>
2019-01-16 20:21:44 +0100

    preview skills in cardview

--- src/components/CardViewFooter.js
+++ src/components/CardViewFooter.js
@@ -1,50 +1,29 @@
 import React, { Component } from 'react';
 
+const defaultSkills = ['HTML', 'CSS', 'GULP'];
+
 class CardViewFooter extends Component {
-    
-    // addSkillsArr(){
-    //     const dataSkills = this.props.dataCard.skills;
-        // if (dataSkills.length  === 0){
-        //     console.log('aquiiii');
-        //     dataSkills.push('HTML', 'CSS', 'GULP');
-        //     dataSkills.map((skill, index) => {
-        //     // return <li key={index} className="skills__item skills__item--bg">{skill}</li>})
-        //     return <div>hola</div>})
-        // } else {
-        // dataSkills.map((skill, index) => {
-        //     console.log(`skill ${skill} el array  ${dataSkills}`);
-        //         return (<li key={index} className="skills__item skills__item--bg">{skill}</li>);
 
-        //     })}
-    // }
-
-    render() {   
-        const dataSkills = this.props.dataCard.skills; 
-        return ( 
+    addSkillsArr() {
+        const dataSkills = this.props.dataCard.skills;
+        if (dataSkills.length === 0) {
+            return (
+                defaultSkills.map((defSkill, index) => {
+                    return <li key={index} className="skills__item skills__item--bg">{defSkill}</li>
+                }))
+        }
+        else {
+            return (dataSkills.map((skill, index) => {
+                return (<li key={index} className="skills__item skills__item--bg">{skill}</li>)
+            }))
+        }
+    }
+    render() {
+        return (
             <div class="card-footer">
                 <div class="skills">
                     <ul class="skills__list">
-
-                    if (dataSkills.length  === 0){
-                        console.log('aquiiii');
-                        dataSkills.push('HTML', 'CSS', 'GULP');
-                        dataSkills.map((skill, index) => {
-                        // return <li key={index} className="skills__item skills__item--bg">{skill}</li>})
-                        return <div>hola</div>})
-                    } else {
-                        dataSkills.map((skill, index) => {
-                        console.log(`skill ${skill} el array  ${dataSkills}`);
-                        return (<li key={index} className="skills__item skills__item--bg">{skill}</li>);
-                        })}
-
-
-
-
-
-
-                        {/* {this.addSkillsArr()} */}
-                        {/* {skills.map((skill, index) => {return (<li key={index} className="skills__item skills__item--bg">{skill}</li>)
-                        })} */}
+                        {this.addSkillsArr()}
                     </ul>
                 </div>
             </div>
