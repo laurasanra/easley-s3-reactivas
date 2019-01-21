099a83d96ded44e9f3cacebea29242da10beb785
inmasalcedo <inma.salcedo.gonzalez@gmail.com>
2019-01-21 20:28:58 +0100

    fix issues merge

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -33,11 +33,7 @@ class Selector extends Component {
 
         <BoxDesign dataCard = {this.props.dataCard} changePalette={this.props.changePalette} changeTypography={this.props.changeTypography} index="1" handleCollapsable={this.handleCollapsable} hideBox={this.hideBox} />
 
-<<<<<<< HEAD
-        <BoxFill dataCard = {this.props.dataCard} backSkills={this.props.backSkills} changeName={this.props.changeName} changeJob={this.props.changeJob} changeEmail={this.props.changeEmail} changePhone={this.props.changePhone} changeLinkedin={this.props.changeLinkedin} changeGithub={this.props.changeGithub} index="2" handleCollapsable={this.handleCollapsable} hideBox={this.hideBox} />
-=======
-        <BoxFill dataCard = {this.props.dataCard} backSkills={this.props.backSkills} changeName={this.props.changeName} changeJob={this.props.changeJob} changeEmail={this.props.changeEmail} changePhone={this.props.changePhone} changeLinkedin={this.props.changeLinkedin} changeGithub={this.props.changeGithub} changeImage={this.props.changeImage} />
->>>>>>> addImage
+        <BoxFill dataCard = {this.props.dataCard} backSkills={this.props.backSkills} changeName={this.props.changeName} changeJob={this.props.changeJob} changeEmail={this.props.changeEmail} changePhone={this.props.changePhone} changeLinkedin={this.props.changeLinkedin} changeGithub={this.props.changeGithub} index="2" handleCollapsable={this.handleCollapsable} hideBox={this.hideBox} changeImage={this.props.changeImage} />
 
         <BoxShare dataCard={this.props.dataCard} index="3" handleCollapsable={this.handleCollapsable} hideBox={this.hideBox} />
       </div>
