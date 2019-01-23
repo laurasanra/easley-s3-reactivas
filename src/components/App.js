c11a043ba84102e2946e25a5acfb3b93c0b54680
emma-martin <emmamartinalvarez@gmail.com>
2019-01-23 19:42:07 +0100

    set React router

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Route, Switch } from 'react-router-dom';
 import CardGenerator from "./CardGenerator";
 
 const defaultData = {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './stylesheets/index.scss';
+import { HashRouter } from 'react-router-dom';
 import App from './components/App.js';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <HashRouter>
+    <App />
+    </HashRouter>, document.getElementById('root'));
