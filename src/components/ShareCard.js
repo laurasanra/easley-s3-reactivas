591455268041608d540b3dbfcc2dd59c6a5e2882
laurasanra <laura.san.rada@gmail.com>
2019-01-28 19:47:49 +0100

    update proptypes

--- src/components/ShareCard.js
+++ src/components/ShareCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class ShareCard extends Component {
     
@@ -31,5 +32,9 @@ class ShareCard extends Component {
     );
 }
 }
+
+ShareCard.propTypes = {
+    URL: PropTypes.string.isRequired
+  }
 
 export default ShareCard;
