607fb90e26f735728bd7fa42fd2e2fd7dcbf506a
emma-martin <emmamartinalvarez@gmail.com>
2019-01-29 15:19:53 +0100

    create docs

---
+++ docs/precache-manifest.9a273f0ec3f23ae35ae9e0009fcb0c88.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f3190f21b6782e415c9cccd3d282277",
+    "url": "./static/media/tiger.6f3190f2.png"
+  },
+  {
+    "revision": "8c760aa5cf9610a31b43",
+    "url": "./static/css/main.c2375ec1.chunk.css"
+  },
+  {
+    "revision": "1fe083cef50171e19412",
+    "url": "./static/js/1.1fe083ce.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "0118e311c7fc2f113bee1abedecf92b2",
+    "url": "./static/media/awesome-card-logo.0118e311.svg"
+  },
+  {
+    "revision": "8c760aa5cf9610a31b43",
+    "url": "./static/js/main.8c760aa5.chunk.js"
+  },
+  {
+    "revision": "7612c1bedbba40ef16ae603d2d2f7921",
+    "url": "./static/media/panter7.7612c1be.png"
+  },
+  {
+    "revision": "7e5be3dd2cb017c7f8dfffbd2a9b95c5",
+    "url": "./static/media/cocodrilo2.7e5be3dd.png"
+  },
+  {
+    "revision": "69380286b9730f9cb4b5a1374013a410",
+    "url": "./static/media/cat2.69380286.png"
+  },
+  {
+    "revision": "1b9fc7b6c1dbbf69daa0a092f2b45662",
+    "url": "./static/media/cebra5.1b9fc7b6.png"
+  },
+  {
+    "revision": "92971c887cf966bc9dd2d5283fed3731",
+    "url": "./index.html"
+  }
+];
